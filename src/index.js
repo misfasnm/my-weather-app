09544da029e9cd49736fb21575d48c86ad7c808c
misfasnm <misfasnm@gmail.com>
2024-03-13 12:10:21 +0000

    API added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {}
+let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+console.log(apiUrl);
+axios.get(apiUrl).then(updateWeather);
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
